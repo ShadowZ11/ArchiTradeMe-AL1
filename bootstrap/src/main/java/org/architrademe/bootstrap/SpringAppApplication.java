b3cb43939b509b76d8ea081ec779438a2a4312f0
Emmanuel <manuboisset@gmail.com>
2023-02-26 01:10:46 +0100

    update java version

--- bootstrap/src/main/java/org/architrademe/bootstrap/SpringAppApplication.java
+++ bootstrap/src/main/java/org/architrademe/bootstrap/SpringAppApplication.java
@@ -3,9 +3,12 @@ package org.architrademe.bootstrap;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+import javax.sql.DataSource;
 
 @ConditionalOnWebApplication
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class SpringAppApplication {
 
     public static void main(String[] args) {
