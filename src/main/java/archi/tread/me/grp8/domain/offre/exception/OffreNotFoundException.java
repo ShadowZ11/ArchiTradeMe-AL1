483438f9a3597a58f672d89baace47e2f24431dc
Audrey <adivito@myges.fr>
2023-02-26 23:20:01 +0100

    End of project

--- src/main/java/archi/tread/me/grp8/domain/offre/exception/OffreNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package archi.tread.me.grp8.domain.offre.exception;
-
-public class OffreNotFoundException extends RuntimeException{
-
-    public OffreNotFoundException(Long id) {
-        super("Offre not found with id : " + id);
-    }
-
-}
